bc7a1663e58f6d23e63334bae461488767eaaf61
felixlinker <felixlinker@outlook.com>
2016-04-19 15:00:36 +0200

    Data completed

--- data/tripleConverter.java
+++ data/tripleConverter.java
@@ -8,11 +8,13 @@ public static void main(String[] args) throws Exception {
 		while ((line = fileReader.readLine()) != null) {
 			output.add(new TripleHelper(line.replaceAll("\n",""),c));
 		}
+		fileReader.close();
 
 		FileWriter fw = new FileWriter("C:/Git/owl2nl.qr-tool/data/instances_verb_new.csv");
 		for (TripleHelper t: output) {
 			fw.append(t.csvLine());
 		}
+		fw.close();
 	}
 
 	public static class TripleHelper {
@@ -32,7 +34,7 @@ public static void main(String[] args) throws Exception {
 			Node third;
 			if (splitted[2].contains("^^")) {
 				String[] literal = (splitted[2]).split("\\^\\^");
-				third = NodeFactory.createLiteral(literal[0], literal[1]);
+				third = NodeFactory.createLiteral(literal[0].split("\"")[1], XSDDatatype.XSDboolean);
 			} else {
 				third = NodeFactory.createURI(splitted[2]);
 			}
