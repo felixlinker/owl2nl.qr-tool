1d55992c40961ee93271aa40f72d250c86e3195f
felixlinker <felixlinker@outlook.com>
2016-04-22 12:20:48 +0200

    Changed amateur to user and expert to experienced user

--- src/main/java/org/aksw/simba/owl2nl/qr/gui/guiHelper/OWL2NL_QRAxiomVerbGuiHelper.java
+++ src/main/java/org/aksw/simba/owl2nl/qr/gui/guiHelper/OWL2NL_QRAxiomVerbGuiHelper.java
@@ -12,7 +12,7 @@ public class OWL2NL_QRAxiomVerbGuiHelper extends OWL2NL_QRGuiHelper<OWL2NL_QRAxi
     public static final String FLUENCY_RATING_NAME = "fluency:";
     public static final String FLUENCY_RATING_KEY = "fluency";
     public static final String EXPERIMENT_IDENTIFIER_VALUE = "OWL2NL_QRAxiomVerb";
-    public static final String EXPERIMENT_IDENTIFIER_NAME = "Axiom verbalization (for experts only)";
+    public static final String EXPERIMENT_IDENTIFIER_NAME = "Axiom verbalization (for experienced users only)";
 
     public static final OWL2NL_QRStarRatingHelper[] STAR_RATINGS = { new OWL2NL_QRStarRatingHelper(ADEQUACY_RATING_KEY, ADEQUACY_RATING_NAME), new OWL2NL_QRStarRatingHelper(FLUENCY_RATING_KEY, FLUENCY_RATING_NAME)};
 
