7841d5627c8d0a90547a1e6cc3a93540b3ab78a6
felixlinker <felixlinker@outlook.com>
2016-05-19 14:15:10 +0200

    DataEvaluater improved

--- src/main/java/org/aksw/simba/owl2nl/qr/tools/DataEvaluater.java
+++ src/main/java/org/aksw/simba/owl2nl/qr/tools/DataEvaluater.java
@@ -5,18 +5,42 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Function;
 
-/**
- * Created by felix on 28.04.2016.
- */
 public class DataEvaluater {
 
     private static final IntegerRowMapper INTEGER_ROW_MAPPER = new IntegerRowMapper();
 
+    private static final String QUERY_COUNT_USERS = "SELECT count(*) FROM Users;";
+    private static final String QUERY_COUNT_USERS_EXPERT = "SELECT count(*) FROM Users WHERE isExpert=1;";
+    private static final String QUERY_COUNT_USERS_USER = "SELECT count(*) FROM Users WHERE isExpert=0;";
 
     public static void main(String[] args) {
         JdbcTemplate jdbcTemplate = DataInserter.getJdbcTemplate();
+
+        List<Integer> results = jdbcTemplate.query(QUERY_COUNT_USERS, INTEGER_ROW_MAPPER);
+        int usersCount = 0;
+        if (!results.isEmpty()) {
+            usersCount = results.get(0);
+        }
+        System.out.println(usersCount + " Users have participated in this experiment.");
+
+        results = jdbcTemplate.query(QUERY_COUNT_USERS_EXPERT, INTEGER_ROW_MAPPER);
+        int usersCountExpert = 0;
+        if (!results.isEmpty()) {
+            usersCountExpert = results.get(0);
+        }
+        System.out.println(usersCountExpert + " of them were experts.");
+
+        results = jdbcTemplate.query(QUERY_COUNT_USERS_USER, INTEGER_ROW_MAPPER);
+        int usersCountUser = 0;
+        if (!results.isEmpty()) {
+            usersCountUser = results.get(0);
+        }
+        System.out.println(usersCountUser + " of them were normal users.");
+
+        System.out.println("That means that " + (usersCount - usersCountExpert - usersCountUser) + " didn't start the evaluation at all.");
+
+        System.out.println();
 
         evaluateAxiomExperiments(jdbcTemplate);
         evaluateResourceExperiments(jdbcTemplate);
@@ -24,7 +48,7 @@ public class DataEvaluater {
     }
 
     // Axiom queries
-    private static final String QUERY_FLUENCY_RATINGS_AXIOM = "SELECT adequacy FROM AxiomExperiments;";
+    private static final String QUERY_FLUENCY_RATINGS_AXIOM = "SELECT fluency FROM AxiomExperiments;";
     private static final String QUERY_ADEQUACY_RATINGS_AXIOM = "SELECT adequacy FROM AxiomExperiments;";
     private static final String QUERY_COUNT_AXIOM_EXPERIMENTS = "SELECT COUNT(*) FROM AxiomExperiments;";
     private static final String QUERY_COUNT_AXIOMS = "SELECT COUNT(*) FROM Axioms;";
@@ -33,36 +57,40 @@ public class DataEvaluater {
         List<Integer> queryResults;
 
         queryResults = jdbcTemplate.query(QUERY_FLUENCY_RATINGS_AXIOM, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        double meanFluencyRating = Double.NaN;
+        double stdDeviationFluencyRating = Double.NaN;
+        if (!queryResults.isEmpty()) {
+            meanFluencyRating = calculateMean(queryResults);
+            stdDeviationFluencyRating = calculateStandardDeviation(queryResults, meanFluencyRating);
         }
 
-        double meanFluencyRating = calculateMean(queryResults);
-        double stdDeviationFluencyRating = calculateStandardDeviation(queryResults, meanFluencyRating);
 
         queryResults = jdbcTemplate.query(QUERY_ADEQUACY_RATINGS_AXIOM, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        double meanAdequacyRating = Double.NaN;
+        double stdDeviationAdequacyRating = Double.NaN;
+        if (!queryResults.isEmpty()) {
+            meanAdequacyRating = calculateMean(queryResults);
+            stdDeviationAdequacyRating = calculateStandardDeviation(queryResults, meanAdequacyRating);
         }
-        double meanAdequacyRating = calculateMean(queryResults);
-        double stdDeviationAdequacyRating = calculateStandardDeviation(queryResults, meanAdequacyRating);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_AXIOMS, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int countAxioms = 0;
+        if (!queryResults.isEmpty()) {
+            countAxioms = queryResults.get(0);
         }
-        int countAxioms = queryResults.get(0);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_AXIOM_EXPERIMENTS, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int countAxiomExperiments = 0;
+        double meanAxiomAnswer = Double.NaN;
+        if (!queryResults.isEmpty()) {
+            countAxiomExperiments = queryResults.get(0);
+            meanAxiomAnswer = (double) countAxiomExperiments / (countAxioms == 0 ? Double.NaN : (double) countAxioms);
         }
-        int countAxiomExeriments = queryResults.get(0);
-        double meanAxiomAnswer =(double)countAxiomExeriments / (double)countAxioms;
 
         System.out.println("Evaluation of adequacy experiments:");
         System.out.println("Mean adequacy is: " + meanAdequacyRating + " with a standard deviation of: " + stdDeviationAdequacyRating);
         System.out.println("Mean fluency is: " + meanFluencyRating + " with a standard deviation of: " + stdDeviationFluencyRating);
+        System.out.println("There are " + countAxiomExperiments + " results.");
         System.out.println("Each axiom has been answered about " + meanAxiomAnswer + " times.");
         System.out.println();
     }
@@ -80,33 +108,38 @@ public class DataEvaluater {
     public static void evaluateResourceExperiments(JdbcTemplate jdbcTemplate) {
         List<Integer> queryResults;
 
+
+        double meanAdequacyExpert = Double.NaN;
+        double stdDeviationAdequacyExpert = Double.NaN;
         queryResults = jdbcTemplate.query(QUERY_ADEQUACY_RATINGS_RESOURCE, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        if (!queryResults.isEmpty()) {
+            meanAdequacyExpert = calculateMean(queryResults);
+            stdDeviationAdequacyExpert = calculateStandardDeviation(queryResults, meanAdequacyExpert);
         }
-        double meanAdequacy = calculateMean(queryResults);
-        double stdDeviationAdequacy = calculateStandardDeviation(queryResults, meanAdequacy);
 
+        double meanCompletenessExpert = Double.NaN;
+        double stdDeviationCompletenessExpert = Double.NaN;
         queryResults = jdbcTemplate.query(QUERY_COMPLETENESS_RATINGS_RESOURCE, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        if (!queryResults.isEmpty()) {
+            meanCompletenessExpert = calculateMean(queryResults);
+            stdDeviationCompletenessExpert = calculateStandardDeviation(queryResults, meanCompletenessExpert);
         }
-        double meanCompleteness = calculateMean(queryResults);
-        double stdDeviationCompleteness = calculateStandardDeviation(queryResults, meanCompleteness);
 
+        double meanFluencyExpert = Double.NaN;
+        double stdDeviationFluencyExpert = Double.NaN;
         List<Integer> fluencyRatingsExpert = jdbcTemplate.query(QUERY_FLUENCY_RATINGS_RESOURCE_EXPERT, INTEGER_ROW_MAPPER);
-        if (fluencyRatingsExpert.isEmpty()) {
-            return;
+        if (!fluencyRatingsExpert.isEmpty()) {
+            meanFluencyExpert = calculateMean(fluencyRatingsExpert);
+            stdDeviationFluencyExpert = calculateStandardDeviation(fluencyRatingsExpert, meanFluencyExpert);
         }
-        double meanFluencyExpert = calculateMean(fluencyRatingsExpert);
-        double stdDeviationFluencyExpert = calculateStandardDeviation(fluencyRatingsExpert, meanFluencyExpert);
 
+        double meanFluencyUser = Double.NaN;
+        double stdDeviationFluencyUser = Double.NaN;
         List<Integer> fluencyRatingsUser = jdbcTemplate.query(QUERY_FLUENCY_RATINGS_RESOURCE_USER, INTEGER_ROW_MAPPER);
-        if (fluencyRatingsUser.isEmpty()) {
-            return;
+        if (!fluencyRatingsUser.isEmpty()) {
+            meanFluencyUser = calculateMean(fluencyRatingsUser);
+            stdDeviationFluencyUser = calculateStandardDeviation(fluencyRatingsUser, meanFluencyUser);
         }
-        double meanFluencyUser = calculateMean(fluencyRatingsUser);
-        double stdDeviationFluencyUser = calculateStandardDeviation(fluencyRatingsUser, meanFluencyUser);
 
         List<Integer> fluencyRatings = fluencyRatingsExpert;
         fluencyRatings.addAll(fluencyRatingsUser);
@@ -114,22 +147,22 @@ public class DataEvaluater {
         double stdDeviationFluency = calculateStandardDeviation(fluencyRatings, meanFluency);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_RESOURCES, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int countResources = 0;
+        if (!queryResults.isEmpty()) {
+            countResources = queryResults.get(0);
         }
-        int countResources = queryResults.get(0);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_USER_RESOURCE_EXPERIMENTS, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int countUserResourceExperiments = 0;
+        if (!queryResults.isEmpty()) {
+            countUserResourceExperiments = queryResults.get(0);
         }
-        int countUserResourceExperiments = queryResults.get(0);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_EXPERT_RESOURCE_EXPERIMENTS, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int countExpertResourceExperiments = 0;
+        if (!queryResults.isEmpty()) {
+            countExpertResourceExperiments = queryResults.get(0);
         }
-        int countExpertResourceExperiments = queryResults.get(0);
 
         double resourceTestedByExpert = (double)countExpertResourceExperiments / (double)countResources;
         double resourceTestedByUser = (double)countUserResourceExperiments / (double)countResources;
@@ -139,12 +172,16 @@ public class DataEvaluater {
         System.out.println("Evaluation of resource experiments:");
         System.out.println("Experts voted on fluency, adequacy and completeness.");
         System.out.println("Mean expert fluency rating: " + meanFluencyExpert + " with a standard deviation of: " + stdDeviationFluencyExpert);
-        System.out.println("Mean expert adequacy rating: " + meanAdequacy + " with a standard deviation of: " + stdDeviationAdequacy);
-        System.out.println("Mean expert completeness rating: " + meanCompleteness + " with a standard deviation of: " + stdDeviationCompleteness);
+        System.out.println("Mean expert adequacy rating: " + meanAdequacyExpert + " with a standard deviation of: " + stdDeviationAdequacyExpert);
+        System.out.println("Mean expert completeness rating: " + meanCompletenessExpert + " with a standard deviation of: " + stdDeviationCompletenessExpert);
+
         System.out.println("Users voted only on fluency.");
         System.out.println("Mean user fluency rating: " + meanFluencyUser + " with a standard deviation of: " + stdDeviationFluencyUser);
         System.out.println("Since both experts and users voted on fluency, we can combine their rating:");
         System.out.println("Mean fluency rating: " + meanFluency + " with a standard deviation of: " + stdDeviationFluency);
+        System.out.println("There are " + countExpertResourceExperiments + " results by experts.");
+        System.out.println("There are " + countUserResourceExperiments + " results by users.");
+        System.out.println("There are " + (countExpertResourceExperiments + countUserResourceExperiments) + " results in total.");
         System.out.println("Each resource has been tested by an user for " + resourceTestedByUser + " times");
         System.out.println("Each resource has been tested by an expert for " + resourceTestedByExpert + " times");
         System.out.println("Each resource has been tested by any for " + resourceTestedByAny + " times");
@@ -162,47 +199,51 @@ public class DataEvaluater {
         List<Integer> queryResults;
 
         queryResults = jdbcTemplate.query(QUERY_CLASS_COUNT_EXPERT, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int classCountExpert = 0;
+        if (!queryResults.isEmpty()) {
+            classCountExpert = queryResults.get(0);
         }
-        int classCountExpert = queryResults.get(0);
 
         queryResults = jdbcTemplate.query(QUERY_CLASS_COUNT_USER, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int classCountUser = 0;
+        if (!queryResults.isEmpty()) {
+            classCountUser = queryResults.get(0);
         }
-        int classCountUser = queryResults.get(0);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_CORRECT_CLASS_EXPERT, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int sumCorrectInstanceExpert = 0;
+        if (!queryResults.isEmpty()) {
+            sumCorrectInstanceExpert = queryResults.get(0);
         }
-        int sumCorrectInstanceExpert = queryResults.get(0);
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_CORRECT_CLASS_USER, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int sumCorrectInstanceUser = 0;
+        if (!queryResults.isEmpty()) {
+            sumCorrectInstanceUser = queryResults.get(0);
         }
-        int sumCorrectInstanceUser = queryResults.get(0);
 
-        double percentageCorrectExpert = ((double)sumCorrectInstanceExpert / (double)classCountExpert) * 100;
-        double percentageCorrectUser = ((double)sumCorrectInstanceUser / (double)classCountUser) * 100;
-        double percentageCorrect = ((double)(sumCorrectInstanceExpert + sumCorrectInstanceUser) / (double)(classCountExpert + classCountUser)) * 100;
+        double percentageCorrectExpert = ((double) sumCorrectInstanceExpert / (classCountExpert == 0 ? Double.NaN : (double) classCountExpert)) * 100;
+        double percentageCorrectUser = ((double) sumCorrectInstanceUser / (classCountUser == 0 ? Double.NaN : (double) classCountUser)) * 100;
+        double percentageCorrect = ((double) (sumCorrectInstanceExpert + sumCorrectInstanceUser) / (classCountExpert + classCountUser == 0 ? Double.NaN : (double) (classCountExpert + classCountUser))) * 100;
 
         queryResults = jdbcTemplate.query(QUERY_COUNT_CLASSES, INTEGER_ROW_MAPPER);
-        if (queryResults.isEmpty()) {
-            return;
+        int countClasses = 0;
+        if (!queryResults.isEmpty()) {
+            countClasses = queryResults.get(0);
         }
-        int countClasses = queryResults.get(0);
 
-        double answeredByExpert = (double)classCountExpert / (double)countClasses;
-        double answeredByUser = (double)classCountUser / (double)countClasses;
-        double answeredByAny = (double)(classCountExpert + classCountUser) / (double)countClasses;
+        double countClassesDouble = countClasses == 0 ? Double.NaN : (double) countClasses;
+        double answeredByExpert = (double) classCountExpert / countClassesDouble;
+        double answeredByUser = (double) classCountUser / countClassesDouble;
+        double answeredByAny = (double) (classCountExpert + classCountUser) / countClassesDouble;
 
         System.out.println("Evaluation of class experiments:");
         System.out.println("Percentage of correctly guessed instances by experts: " + percentageCorrectExpert);
         System.out.println("Percentage of correctly guessed instances by users: " + percentageCorrectUser);
         System.out.println("Percentage of correctly guessed instances overall: " + percentageCorrect);
+        System.out.println("There are " + classCountExpert + " results by experts.");
+        System.out.println("There are " + classCountUser + " results by users.");
+        System.out.println("There are " + (classCountExpert + classCountUser) + " results in total.");
         System.out.println("Each class has been tested by an expert for " + answeredByExpert + " times");
         System.out.println("Each class has been tested by an user for " + answeredByUser + " times");
         System.out.println("Each class has been tested by any for " + answeredByAny + " times");
